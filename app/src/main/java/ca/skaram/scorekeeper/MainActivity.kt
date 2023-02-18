7a83f40db3a1490aac8a2b446fae8fd02a50f9fa
skaram20c <skaram20c@gmail.com>
2023-02-18 02:32:02 -0500

    Added XML documentation and comments

--- app/src/main/java/ca/skaram/scorekeeper/MainActivity.kt
+++ app/src/main/java/ca/skaram/scorekeeper/MainActivity.kt
@@ -2,10 +2,19 @@ package ca.skaram.scorekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.dec_button).setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        Log.i("OnClick", "A button was pressed")
     }
 }
