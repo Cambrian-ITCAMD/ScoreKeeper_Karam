8d7ef2e6a8f03d97706fea424a48c7bbb287962f
skaram20c <skaram20c@gmail.com>
2023-03-03 20:32:43 -0500

    update

--- app/src/main/java/ca/skaram/scorekeeper/MainActivity.kt
+++ app/src/main/java/ca/skaram/scorekeeper/MainActivity.kt
@@ -7,14 +7,27 @@ import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var teamAPoints = 0
+    private var teamBPoints = 0
+    private var pointsChangeBy = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.dec_button).setOnClickListener(this)
+        findViewById< Button>(R.id.inc_button).setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
         Log.i("OnClick", "A button was pressed")
+    }
+
+    private fun incPoints(){
+
+    }
+
+    private fun decPoints(){
+
     }
 }
