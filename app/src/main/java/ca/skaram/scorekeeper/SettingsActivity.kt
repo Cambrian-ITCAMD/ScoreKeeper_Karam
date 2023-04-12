bfb7ecffbb6ca266333dafa721b758ba83424fab
skaram20c <skaram20c@gmail.com>
2023-04-12 19:43:47 -0400

    added toast to display mssg when sava data switch is pressed

--- app/src/main/java/ca/skaram/scorekeeper/SettingsActivity.kt
+++ app/src/main/java/ca/skaram/scorekeeper/SettingsActivity.kt
@@ -1,12 +1,13 @@
 package ca.skaram.scorekeeper
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 
-class SettingsActivity : AppCompatActivity() {
-
+class SettingsActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
@@ -19,9 +20,30 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
+
+        private lateinit var mySwitchPreference: SwitchPreference
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.settings_layout, rootKey)
+            mySwitchPreference = findPreference("switch_preference_1")!!
+            mySwitchPreference.onPreferenceChangeListener = this
+        }
+
+        /**
+         * Called when a preference has been changed by the user. This is called before the state
+         * of the preference is about to be updated and before the state is persisted.
+         *
+         * @param preference The changed preference
+         * @param newValue   The new value of the preference
+         * @return `true` to update the state of the preference with the new value
+         */
+        override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+            if (preference == mySwitchPreference) {
+                val switchedOn = newValue as Boolean
+                val toastMsg = if (switchedOn) "Save Data mode: ON" else "Save Data mode: OFF"
+                Toast.makeText(requireContext(), toastMsg, Toast.LENGTH_LONG).show()
+            }
+            return true
         }
     }
 }
